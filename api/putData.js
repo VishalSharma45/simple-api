5922f6126a43882123296acc788614917f0871e3
Vishal Sharma <invincible454@gmail.com>
2024-02-13 23:48:38 +0530

    wip

---
+++ api/putData.js
@@ -0,0 +1,17 @@
+const getConnect = require('./connection');
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.put('/:name', async (req, res) => {
+    let data = await getConnect();
+    let result = await data.updateOne(
+        // { name: req.body.name },
+        { name: req.params.name },
+        { $set: req.body })
+    console.log(result)
+    // res.send();
+})
+
+app.listen(5500);
